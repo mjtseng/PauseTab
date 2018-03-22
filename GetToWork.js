e7fb20c1cb201e6103d9b234c30130411b940f15
Maggie Tseng <mjtseng42@gmail.com>
2018-03-21 21:34:24 -0400

    added GetToWork.js for desktop notifications

---
+++ GetToWork.js
@@ -0,0 +1,24 @@
+//javascript for desktop notifications
+
+/*
+
+the following code was copied and pasted from Eunoia (https://github.com/VTam25/Eunoia/blob/master/s1.js)
+I switched out the words and kept the syntax. I also deleted some unnecessary whitespace.
+This code should be carefully reviewed before being finalized.
+
+var options= 
+{
+	type:"basic",
+	title:"Stop procrastinating!",
+	message:"It's time to stop procrastinating! Get to work, or take a mental break",
+	iconUrl:"./assets/notification.png" //there is currently no icon created for PauseTab. When made, it'll be placed in "assets"
+};
+
+chrome.notifications.create(options, callback);
+	console.log("Popup done!");
+
+function callback(){
+
+}
+
+*/
